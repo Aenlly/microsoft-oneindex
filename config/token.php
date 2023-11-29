6dbbc0d9bb13371a9eee0e6e1a514849ae170079
Aenlly <2645274834@qq.com>
2023-11-29 16:33:28 +0800

    feat: git init

---
+++ config/token.php
@@ -0,0 +1,9 @@
+<?php return array(
+    'token_type' => 'Bearer',
+    'scope' => 'Files.ReadWrite.All User.Read profile openid email',
+    'expires_in' => 5361,
+    'ext_expires_in' => 5361,
+    'access_token' => '',
+    'refresh_token' => '',
+    'expires_on' => 1701248328,
+);
